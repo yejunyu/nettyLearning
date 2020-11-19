d052eb4d4d26eca703af0ef38ffdb4bc659e2c50
yejunyu <liuxing_yue@126.com>
2020-11-19 09:36:46 +0800

    1. NIO

---
+++ RPCdemo/src/main/java/com/yejunyu/NioTest.java
@@ -0,0 +1,26 @@
+package com.yejunyu;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author: YeJunyu
+ * @description:
+ * @email: yyyejunyu@gmail.com
+ * @date: 2020/11/18
+ */
+public class NioTest {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // 多个 port
+
+        // selector
+
+        // channel 注册到 selector 上,selector 关注 accept 事件
+
+        // 循环接收 selector 事件
+
+        // 如果是连接事件
+
+        // 如果是可读事件
+    }
+}
